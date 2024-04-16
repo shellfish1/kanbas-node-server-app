567861d534bc47f433c1827f4247fee792d376bf
shellfish1 <manikondanikhil@gmail.com>
2024-04-15 21:00:43 -0400

    Updating modules and assignments logic

--- Kanbas/Database/assignments.js
+++ Kanbas/Database/assignments.js
@@ -1,20 +1,254 @@
 export default [
-  { "_id": "A101", "startDate": "2024-02-28T10:00:00" ,"description" : "", "title": "Propulsion Assignment", "course": "RS101", "maxScore" : 100, "dueDate": "2024-02-28T10:00:00" },
-  { "_id": "A102", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Combustion Analysis", "course": "RS101", "maxScore" : 100, "dueDate": "2024-02-29T08:00:00" },
-  { "_id": "A103", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Nozzle Design Project", "course": "RS101", "maxScore" : 100, "dueDate": "2024-02-27T15:00:00" },
-  { "_id": "A104", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Thermal Analysis", "course": "RS101", "maxScore" : 100, "dueDate": "2024-03-21T14:00:00" },
-  { "_id": "A105", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Fluid Dynamics Assignment", "course": "RS101", "maxScore" : 100, "dueDate": "2024-03-22T16:00:00" },
-  { "_id": "A106", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Rocket Design Project", "course": "RS101", "maxScore" : 100, "dueDate": "2024-03-23T12:00:00" },
-  { "_id": "A201", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Aerodynamics Quiz", "course": "RS102", "maxScore" : 100, "dueDate": "2024-02-25T20:00:00" },
-  { "_id": "A202", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Flow Analysis", "course": "RS102", "maxScore" : 100, "dueDate": "2024-02-24T14:00:00" },
-  { "_id": "A203", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Heating Analysis", "course": "RS102", "maxScore" : 100,  "dueDate": "2024-02-27T18:00:00" },
-  { "_id": "A204", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Pressure Dynamics Assignment", "course": "RS102", "maxScore" : 100, "dueDate": "2024-03-20T10:00:00" },
-  { "_id": "A205", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Wind Tunnel Experiment", "course": "RS102", "maxScore" : 100, "dueDate": "2024-03-21T09:00:00" },
-  { "_id": "A206", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Turbulence Analysis", "course": "RS102", "maxScore" : 100, "dueDate": "2024-03-22T15:00:00" },
-  { "_id": "A301", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Structural Design Task", "course": "RS103", "maxScore" : 100, "dueDate": "2024-02-23T11:00:00" },
-  { "_id": "A302", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Orbital Calculations", "course": "RS103", "maxScore" : 100, "dueDate": "2024-02-25T09:00:00" },
-  { "_id": "A303", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Systems Engineering Exam", "course": "RS103", "maxScore" : 100, "dueDate": "2024-02-28T16:00:00" },
-  { "_id": "A304", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Satellite Design Project", "course": "RS103", "maxScore" : 100, "dueDate": "2024-03-21T12:00:00" },
-  { "_id": "A305", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Spacecraft Propulsion Assignment", "course": "RS103", "maxScore" : 100, "dueDate": "2024-03-22T14:00:00" },
-  { "_id": "A306", "startDate": "2024-02-28T10:00:00" , "description" : "", "title": "Rocket Launch Simulation", "course": "RS103", "maxScore" : 100, "dueDate": "2024-03-23T08:00:00" }
+  {
+    "_id": "65a7fcb7a11aca237708374c",
+    "title": "Propulsion Assignment",
+    "course": "661a1684da84587c07817fff",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "6d3f8e2a80e18651d216c1b9",
+    "title": "Combustion Analysis",
+    "course": "661a1684da84587c07817fff",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "ac21b2ef84059b219edcc4d8",
+    "title": "Nozzle Design Project",
+    "course": "661a1684da84587c07817fff",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "52f4747156c4711cc5c2486e",
+    "title": "Airfoil Experiment",
+    "course": "661a1684da84587c07818000",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "12d39f0bb469ec23c8cf3ec2",
+    "title": "Drag Coefficient Calculation",
+    "course": "661a1684da84587c07818000",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "0e67d20999759d81a01f5d2c",
+    "title": "Structural Analysis",
+    "course": "661a1684da84587c07818001",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "1b1e69d681b3de8ff336b4e6",
+    "title": "Thermal Control Project",
+    "course": "661a1684da84587c07818001",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "c38e03b9b414f92a6e7af90f",
+    "title": "Payload Integration Assignment",
+    "course": "661a1684da84587c07818001",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "5ae6fc0d4b186c71dc23e0fb",
+    "title": "Organic Reactions Lab",
+    "course": "661a1684da84587c07818002",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "f5a469d93a4d41be0c91ebf2",
+    "title": "Spectroscopy Assignment",
+    "course": "661a1684da84587c07818002",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "d87aa755de8e60fa7bcb49e6",
+    "title": "Inorganic Synthesis Project",
+    "course": "661a1684da84587c07818003",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "57cb177ad4a9db6fd6edf7ab",
+    "title": "Coordination Chemistry Report",
+    "course": "661a1684da84587c07818003",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "24a937a62c9fbc2f0d201c9a",
+    "title": "Kinetics Experiment",
+    "course": "661a1684da84587c07818004",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "7a3fca0f0d8a749ef6d64d97",
+    "title": "Quantum Mechanics Problem Set",
+    "course": "661a1684da84587c07818004",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "a35a3f2da0ef2a98d7dc2f5d",
+    "title": "Elvish Linguistics Essay",
+    "course": "661a1684da84587c07818005",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "6a312e1a45e5ef2c8e09cbf9",
+    "title": "Diplomatic Case Study",
+    "course": "661a1684da84587c07818006",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "78d040b90b5f22e91e165ee0",
+    "title": "Magic Ritual Analysis",
+    "course": "661a1684da84587c07818007",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "1b5bc19c0a5cbb9c8db5e471",
+    "title": "Democratic Education Case Study",
+    "course": "661a1684da84587c07818008",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "1a7cfb0e887f292e9b31d7ab",
+    "title": "Independence Philosophy Essay",
+    "course": "661a1684da84587c07818009",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "02cf1dbd836e48d5a93db5e0",
+    "title": "Early Education Innovations Presentation",
+    "course": "661a1684da84587c0781800a",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "6e940507f8a7db997c89e29a",
+    "title": "Ethical Dilemmas Discussion",
+    "course": "661a1684da84587c0781800b",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "a2e4b9c9cbe3e1ef4167ae19",
+    "title": "Apostolic Age Analysis",
+    "course": "661a1684da84587c0781800c",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "35c04bfb738d54e29233d6fd",
+    "title": "Christian Leadership Reflection",
+    "course": "661a1684da84587c0781800d",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "2ec3b3f7c6a53e377bc7e6b7",
+    "title": "Sufi Poetry Interpretation",
+    "course": "661a1684da84587c0781800e",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "025dbf1e8c44e17c1b481268",
+    "title": "Islamic Ethics Case Study",
+    "course": "661a1684da84587c0781800f",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "52f5bea998ed3de2946faa32",
+    "title": "Esotericism Essay",
+    "course": "661a1684da84587c07818010",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "1e52787ddca3e7138ad40526",
+    "title": "Ancient Chinese Agriculture Project",
+    "course": "661a1684da84587c07818011",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  },
+  {
+    "_id": "3ae7a0d95a7a8d868b847903",
+    "title": "Leadership in Mythology Analysis",
+    "course": "661a1684da84587c07818012",
+    "startDate": "2024-02-28T10:00:00",
+    "dueDate": "2024-03-01T10:00:00",
+    "maxScore": 100,
+    "description": ""
+  }
 ]
--- Kanbas/Database/grades.js
+++ Kanbas/Database/grades.js
@@ -1,650 +1,170 @@
 export default [
   {
-    "_id": "1",
+    "_id": "65a7fcb7a11aca237708374c",
     "student": "121",
-    "assignment": "A101",
+    "assignment": "65a7fcb7a11aca237708374c",
     "grade": 85
   },
   {
-    "_id": "2",
+    "_id": "6d3f8e2a80e18651d216c1b9",
     "student": "121",
-    "assignment": "A102",
-    "grade": 90
+    "assignment": "6d3f8e2a80e18651d216c1b9",
+    "grade": 78
   },
   {
-    "_id": "3",
+    "_id": "ac21b2ef84059b219edcc4d8",
     "student": "121",
-    "assignment": "A103",
-    "grade": 80
-  },
-  {
-    "_id": "4",
-    "student": "121",
-    "assignment": "A104",
-    "grade": 88
-  },
-  {
-    "_id": "5",
-    "student": "121",
-    "assignment": "A105",
-    "grade": 82
-  },
-  {
-    "_id": "6",
-    "student": "121",
-    "assignment": "A106",
-    "grade": 91
-  },
-  {
-    "_id": "7",
-    "student": "121",
-    "assignment": "A201",
-    "grade": 87
-  },
-  {
-    "_id": "8",
-    "student": "121",
-    "assignment": "A202",
+    "assignment": "ac21b2ef84059b219edcc4d8",
     "grade": 92
   },
   {
-    "_id": "9",
-    "student": "121",
-    "assignment": "A203",
-    "grade": 84
-  },
-  {
-    "_id": "10",
-    "student": "121",
-    "assignment": "A204",
-    "grade": 89
-  },
-  {
-    "_id": "11",
-    "student": "121",
-    "assignment": "A205",
-    "grade": 85
-  },
-  {
-    "_id": "12",
-    "student": "121",
-    "assignment": "A206",
-    "grade": 90
-  },
-  {
-    "_id": "13",
+    "_id": "52f4747156c4711cc5c2486e",
     "student": "122",
-    "assignment": "A101",
+    "assignment": "52f4747156c4711cc5c2486e",
+    "grade": 70
+  },
+  {
+    "_id": "12d39f0bb469ec23c8cf3ec2",
+    "student": "122",
+    "assignment": "12d39f0bb469ec23c8cf3ec2",
     "grade": 88
   },
   {
-    "_id": "14",
-    "student": "122",
-    "assignment": "A102",
-    "grade": 92
+    "_id": "0e67d20999759d81a01f5d2c",
+    "student": "123",
+    "assignment": "0e67d20999759d81a01f5d2c",
+    "grade": 95
   },
   {
-    "_id": "15",
-    "student": "122",
-    "assignment": "A103",
+    "_id": "1b1e69d681b3de8ff336b4e6",
+    "student": "123",
+    "assignment": "1b1e69d681b3de8ff336b4e6",
     "grade": 82
   },
   {
-    "_id": "16",
-    "student": "122",
-    "assignment": "A104",
+    "_id": "c38e03b9b414f92a6e7af90f",
+    "student": "124",
+    "assignment": "c38e03b9b414f92a6e7af90f",
+    "grade": 75
+  },
+  {
+    "_id": "5ae6fc0d4b186c71dc23e0fb",
+    "student": "124",
+    "assignment": "5ae6fc0d4b186c71dc23e0fb",
     "grade": 90
   },
   {
-    "_id": "17",
-    "student": "122",
-    "assignment": "A105",
+    "_id": "f5a469d93a4d41be0c91ebf2",
+    "student": "125",
+    "assignment": "f5a469d93a4d41be0c91ebf2",
     "grade": 83
   },
   {
-    "_id": "18",
-    "student": "122",
-    "assignment": "A106",
-    "grade": 89
+    "_id": "d87aa755de8e60fa7bcb49e6",
+    "student": "125",
+    "assignment": "d87aa755de8e60fa7bcb49e6",
+    "grade": 88
   },
   {
-    "_id": "19",
+    "_id": "57cb177ad4a9db6fd6edf7ab",
+    "student": "126",
+    "assignment": "57cb177ad4a9db6fd6edf7ab",
+    "grade": 79
+  },
+  {
+    "_id": "24a937a62c9fbc2f0d201c9a",
+    "student": "126",
+    "assignment": "24a937a62c9fbc2f0d201c9a",
+    "grade": 91
+  },
+  {
+    "_id": "7a3fca0f0d8a749ef6d64d97",
+    "student": "127",
+    "assignment": "7a3fca0f0d8a749ef6d64d97",
+    "grade": 87
+  },
+  {
+    "_id": "a35a3f2da0ef2a98d7dc2f5d",
+    "student": "127",
+    "assignment": "a35a3f2da0ef2a98d7dc2f5d",
+    "grade": 94
+  },
+  {
+    "_id": "6a312e1a45e5ef2c8e09cbf9",
+    "student": "128",
+    "assignment": "6a312e1a45e5ef2c8e09cbf9",
+    "grade": 80
+  },
+  {
+    "_id": "78d040b90b5f22e91e165ee0",
+    "student": "128",
+    "assignment": "78d040b90b5f22e91e165ee0",
+    "grade": 85
+  },
+  {
+    "_id": "1b5bc19c0a5cbb9c8db5e471",
+    "student": "129",
+    "assignment": "1b5bc19c0a5cbb9c8db5e471",
+    "grade": 73
+  },
+  {
+    "_id": "1a7cfb0e887f292e9b31d7ab",
+    "student": "129",
+    "assignment": "1a7cfb0e887f292e9b31d7ab",
+    "grade": 90
+  },
+  {
+    "_id": "02cf1dbd836e48d5a93db5e0",
+    "student": "121",
+    "assignment": "02cf1dbd836e48d5a93db5e0",
+    "grade": 88
+  },
+  {
+    "_id": "6e940507f8a7db997c89e29a",
     "student": "122",
-    "assignment": "A201",
+    "assignment": "6e940507f8a7db997c89e29a",
+    "grade": 75
+  },
+  {
+    "_id": "a2e4b9c9cbe3e1ef4167ae19",
+    "student": "123",
+    "assignment": "a2e4b9c9cbe3e1ef4167ae19",
+    "grade": 85
+  },
+  {
+    "_id": "35c04bfb738d54e29233d6fd",
+    "student": "124",
+    "assignment": "35c04bfb738d54e29233d6fd",
+    "grade": 92
+  },
+  {
+    "_id": "2ec3b3f7c6a53e377bc7e6b7",
+    "student": "125",
+    "assignment": "2ec3b3f7c6a53e377bc7e6b7",
+    "grade": 79
+  },
+  {
+    "_id": "025dbf1e8c44e17c1b481268",
+    "student": "126",
+    "assignment": "025dbf1e8c44e17c1b481268",
     "grade": 86
   },
   {
-    "_id": "20",
-    "student": "122",
-    "assignment": "A202",
-    "grade": 91
+    "_id": "52f5bea998ed3de2946faa32",
+    "student": "127",
+    "assignment": "52f5bea998ed3de2946faa32",
+    "grade": 93
   },
   {
-    "_id": "21",
-    "student": "122",
-    "assignment": "A203",
-    "grade": 85
-  },
-  {
-    "_id": "22",
-    "student": "122",
-    "assignment": "A204",
-    "grade": 88
-  },
-  {
-    "_id": "23",
-    "student": "122",
-    "assignment": "A205",
+    "_id": "1e52787ddca3e7138ad40526",
+    "student": "128",
+    "assignment": "1e52787ddca3e7138ad40526",
     "grade": 84
   },
   {
-    "_id": "24",
-    "student": "122",
-    "assignment": "A206",
-    "grade": 92
-  },
-  {
-    "_id": "25",
-    "student": "123",
-    "assignment": "A101",
-    "grade": 87
-  },
-  {
-    "_id": "26",
-    "student": "123",
-    "assignment": "A102",
-    "grade": 89
-  },
-  {
-    "_id": "27",
-    "student": "123",
-    "assignment": "A103",
-    "grade": 81
-  },
-  {
-    "_id": "28",
-    "student": "123",
-    "assignment": "A104",
-    "grade": 87
-  },
-  {
-    "_id": "29",
-    "student": "123",
-    "assignment": "A105",
-    "grade": 80
-  },
-  {
-    "_id": "30",
-    "student": "123",
-    "assignment": "A106",
-    "grade": 88
-  },
-  {
-    "_id": "31",
-    "student": "123",
-    "assignment": "A201",
-    "grade": 85
-  },
-  {
-    "_id": "32",
-    "student": "123",
-    "assignment": "A202",
-    "grade": 90
-  },
-  {
-    "_id": "33",
-    "student": "123",
-    "assignment": "A203",
-    "grade": 83
-  },
-  {
-    "_id": "34",
-    "student": "123",
-    "assignment": "A204",
-    "grade": 89
-  },
-  {
-    "_id": "35",
-    "student": "123",
-    "assignment": "A205",
-    "grade": 86
-  },
-  {
-    "_id": "36",
-    "student": "123",
-    "assignment": "A206",
-    "grade": 91
-  },
-  {
-    "_id": "1",
-    "student": "121",
-    "assignment": "A301",
-    "grade": 85
-  },
-  {
-    "_id": "2",
-    "student": "121",
-    "assignment": "A302",
-    "grade": 90
-  },
-  {
-    "_id": "3",
-    "student": "121",
-    "assignment": "A303",
-    "grade": 80
-  },
-  {
-    "_id": "4",
-    "student": "121",
-    "assignment": "A304",
-    "grade": 88
-  },
-  {
-    "_id": "5",
-    "student": "121",
-    "assignment": "A305",
-    "grade": 82
-  },
-  {
-    "_id": "6",
-    "student": "121",
-    "assignment": "A306",
-    "grade": 91
-  },
-  {
-    "_id": "7",
-    "student": "122",
-    "assignment": "A301",
-    "grade": 87
-  },
-  {
-    "_id": "8",
-    "student": "122",
-    "assignment": "A302",
-    "grade": 92
-  },
-  {
-    "_id": "9",
-    "student": "122",
-    "assignment": "A303",
-    "grade": 84
-  },
-  {
-    "_id": "10",
-    "student": "122",
-    "assignment": "A304",
-    "grade": 89
-  },
-  {
-    "_id": "11",
-    "student": "122",
-    "assignment": "A305",
-    "grade": 85
-  },
-  {
-    "_id": "12",
-    "student": "122",
-    "assignment": "A306",
-    "grade": 90
-  },
-  {
-    "_id": "13",
-    "student": "123",
-    "assignment": "A301",
-    "grade": 88
-  },
-  {
-    "_id": "14",
-    "student": "123",
-    "assignment": "A302",
-    "grade": 92
-  },
-  {
-    "_id": "15",
-    "student": "123",
-    "assignment": "A303",
-    "grade": 82
-  },
-  {
-    "_id": "16",
-    "student": "123",
-    "assignment": "A304",
-    "grade": 90
-  },
-  {
-    "_id": "17",
-    "student": "123",
-    "assignment": "A305",
-    "grade": 83
-  },
-  {
-    "_id": "18",
-    "student": "123",
-    "assignment": "A306",
-    "grade": 89
-  },
-  {
-    "_id": "19",
-    "student": "124",
-    "assignment": "A301",
-    "grade": 85
-  },
-  {
-    "_id": "20",
-    "student": "124",
-    "assignment": "A302",
-    "grade": 90
-  },
-  {
-    "_id": "21",
-    "student": "124",
-    "assignment": "A303",
-    "grade": 80
-  },
-  {
-    "_id": "22",
-    "student": "124",
-    "assignment": "A304",
-    "grade": 88
-  },
-  {
-    "_id": "23",
-    "student": "124",
-    "assignment": "A305",
-    "grade": 82
-  },
-  {
-    "_id": "24",
-    "student": "124",
-    "assignment": "A306",
-    "grade": 91
-  },
-  {
-    "_id": "25",
-    "student": "125",
-    "assignment": "A301",
-    "grade": 87
-  },
-  {
-    "_id": "26",
-    "student": "125",
-    "assignment": "A302",
-    "grade": 92
-  },
-  {
-    "_id": "27",
-    "student": "125",
-    "assignment": "A303",
-    "grade": 84
-  },
-  {
-    "_id": "28",
-    "student": "125",
-    "assignment": "A304",
-    "grade": 89
-  },
-  {
-    "_id": "29",
-    "student": "125",
-    "assignment": "A305",
-    "grade": 85
-  },
-  {
-    "_id": "30",
-    "student": "125",
-    "assignment": "A306",
-    "grade": 90
-  },
-  {
-    "_id": "31",
-    "student": "126",
-    "assignment": "A301",
-    "grade": 88
-  },
-  {
-    "_id": "32",
-    "student": "126",
-    "assignment": "A302",
-    "grade": 92
-  },
-  {
-    "_id": "33",
-    "student": "126",
-    "assignment": "A303",
-    "grade": 82
-  },
-  {
-    "_id": "34",
-    "student": "126",
-    "assignment": "A304",
-    "grade": 90
-  },
-  {
-    "_id": "35",
-    "student": "126",
-    "assignment": "A305",
-    "grade": 83
-  },
-  {
-    "_id": "36",
-    "student": "126",
-    "assignment": "A306",
-    "grade": 89
-  },
-  {
-    "_id": "1",
-    "student": "124",
-    "assignment": "A101",
-    "grade": 85
-  },
-  {
-    "_id": "2",
-    "student": "124",
-    "assignment": "A102",
-    "grade": 90
-  },
-  {
-    "_id": "3",
-    "student": "124",
-    "assignment": "A103",
-    "grade": 80
-  },
-  {
-    "_id": "4",
-    "student": "124",
-    "assignment": "A104",
-    "grade": 88
-  },
-  {
-    "_id": "5",
-    "student": "124",
-    "assignment": "A105",
-    "grade": 82
-  },
-  {
-    "_id": "6",
-    "student": "124",
-    "assignment": "A106",
-    "grade": 91
-  },
-  {
-    "_id": "7",
-    "student": "125",
-    "assignment": "A101",
-    "grade": 87
-  },
-  {
-    "_id": "8",
-    "student": "125",
-    "assignment": "A102",
-    "grade": 92
-  },
-  {
-    "_id": "9",
-    "student": "125",
-    "assignment": "A103",
-    "grade": 84
-  },
-  {
-    "_id": "10",
-    "student": "125",
-    "assignment": "A104",
-    "grade": 89
-  },
-  {
-    "_id": "11",
-    "student": "125",
-    "assignment": "A105",
-    "grade": 85
-  },
-  {
-    "_id": "12",
-    "student": "125",
-    "assignment": "A106",
-    "grade": 90
-  },
-  {
-    "_id": "13",
-    "student": "126",
-    "assignment": "A101",
-    "grade": 88
-  },
-  {
-    "_id": "14",
-    "student": "126",
-    "assignment": "A102",
-    "grade": 92
-  },
-  {
-    "_id": "15",
-    "student": "126",
-    "assignment": "A103",
-    "grade": 82
-  },
-  {
-    "_id": "16",
-    "student": "126",
-    "assignment": "A104",
-    "grade": 90
-  },
-  {
-    "_id": "17",
-    "student": "126",
-    "assignment": "A105",
-    "grade": 83
-  },
-  {
-    "_id": "18",
-    "student": "126",
-    "assignment": "A106",
-    "grade": 89
-  },
-  {
-    "_id": "1",
-    "student": "124",
-    "assignment": "A201",
-    "grade": 85
-  },
-  {
-    "_id": "2",
-    "student": "124",
-    "assignment": "A202",
-    "grade": 90
-  },
-  {
-    "_id": "3",
-    "student": "124",
-    "assignment": "A203",
-    "grade": 80
-  },
-  {
-    "_id": "4",
-    "student": "124",
-    "assignment": "A204",
-    "grade": 88
-  },
-  {
-    "_id": "5",
-    "student": "124",
-    "assignment": "A205",
-    "grade": 82
-  },
-  {
-    "_id": "6",
-    "student": "124",
-    "assignment": "A206",
-    "grade": 91
-  },
-  {
-    "_id": "7",
-    "student": "125",
-    "assignment": "A201",
-    "grade": 87
-  },
-  {
-    "_id": "8",
-    "student": "125",
-    "assignment": "A202",
-    "grade": 92
-  },
-  {
-    "_id": "9",
-    "student": "125",
-    "assignment": "A203",
-    "grade": 84
-  },
-  {
-    "_id": "10",
-    "student": "125",
-    "assignment": "A204",
-    "grade": 89
-  },
-  {
-    "_id": "11",
-    "student": "125",
-    "assignment": "A205",
-    "grade": 85
-  },
-  {
-    "_id": "12",
-    "student": "125",
-    "assignment": "A206",
-    "grade": 90
-  },
-  {
-    "_id": "13",
-    "student": "126",
-    "assignment": "A201",
-    "grade": 88
-  },
-  {
-    "_id": "14",
-    "student": "126",
-    "assignment": "A202",
-    "grade": 92
-  },
-  {
-    "_id": "15",
-    "student": "126",
-    "assignment": "A203",
-    "grade": 82
-  },
-  {
-    "_id": "16",
-    "student": "126",
-    "assignment": "A204",
-    "grade": 90
-  },
-  {
-    "_id": "17",
-    "student": "126",
-    "assignment": "A205",
-    "grade": 83
-  },
-  {
-    "_id": "18",
-    "student": "126",
-    "assignment": "A206",
-    "grade": 89
+    "_id": "3ae7a0d95a7a8d868b847903",
+    "student": "129",
+    "assignment": "3ae7a0d95a7a8d868b847903",
+    "grade": 77
   }
 ]
